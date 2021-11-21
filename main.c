370ae083ef616f98f4dc2b62f3d4d746d0747c7b
Mavridis Philippe <mavridisf@gmail.com>
2021-11-21 09:39:12 +0200

    Fixed identation in source files.
    Signed-off-by: Mavridis Philippe <mavridisf@gmail.com>

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /***************************
- * Name:       main.c      *
- * Programmer: blu.256     *
- * Date:       2021/11/20  *
- ***************************/
+* Name:       main.c      *
+* Programmer: blu.256     *
+* Date:       2021/11/20  *
+***************************/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -60,7 +60,7 @@ int main( int argc, char* argv[] )
 
 	term_setup();
 	reset_ptr();
-    rand_init();
+	rand_init();
 
 	FILE *source = fopen(argv[1], "r");
 
@@ -102,97 +102,95 @@ int main( int argc, char* argv[] )
 				putchar(*memptr);
 				break;
 
-            case 64: // @ goto
-                fseek(source, *memptr, SEEK_SET);
-                break;
+			case 64: // @ goto
+				fseek(source, *memptr, SEEK_SET);
+				break;
 
-            case 95: // _ jump to first | to the left
-            {
-                char tmp = 0;
-                int  old = ftell(source);
-                while( fseek(source, -2, SEEK_CUR) == 0 )
-                {
-                    tmp = fgetc(source);
-                    if( tmp == 124 )
-                        break;
-                }
+			case 95: // _ jump to first | to the left
+			{
+				char tmp = 0;
+				int  old = ftell(source);
+				while( fseek(source, -2, SEEK_CUR) == 0 )
+				{
+					tmp = fgetc(source);
+					if( tmp == 124 )
+						break;
+				}
 
-                if( tmp != 124 )
-                    fseek(source, old, SEEK_SET);
+				if( tmp != 124 )
+					fseek(source, old, SEEK_SET);
 
-                break;
+				break;
+			}
+
+			case 61: // = jump to first | to the right
+			{
+				char tmp = 0;
+				int  old = ftell(source);
+				while(! feof(source) )
+				{
+					tmp = getc(source);
+			
+					if( tmp == 124 )
+						break;
+				}
+
+				if( tmp != 124 )
+					fseek(source, old, SEEK_SET);
+
+				break;
+			}
+
+			case 35: // # jump to | by value
+			{
+				char tmp       = 0;
+				int  occurence = 0;
+				int  old       = ftell(source);
+
+				fseek(source, 0, SEEK_SET);
+				while(! feof(source) )
+				{
+					tmp = getc(source);
+
+					if( tmp == 124 )
+					{
+						++occurence;
+						if( occurence == *memptr )
+							break;
+					}
+				}
+
+				if( occurence == 0 || occurence != *memptr )
+					fseek(source, old, SEEK_SET);
+
+				break;
             }
 
-            case 61: // = jump to first | to the right
-            {
-                char tmp = 0;
-                int  old = ftell(source);
-                while(! feof(source) )
-                {
-			tmp = getc(source);
-			
-                    if( tmp == 124 )
-                        break;
-                }
+			case 94: // ^ seek
+				fseek(source, *memptr, SEEK_CUR);
+				break;
 
-                if( tmp != 124 )
-                    fseek(source, old, SEEK_SET);
+			case 38: // & if zero, skip next instruction
+				if(!*memptr)
+					fseek(source, 1, SEEK_CUR);
+				break;
 
-                break;
-            }
+			case 96: // ` if zero, exit
+				if(!*memptr)
+				{
+					cleanup();
+					return 0;
+				}
+				break;
 
-            case 35: // # jump to | by value
-            {
-                char tmp       = 0;
-                int  occurence = 0;
-                int  old       = ftell(source);
-
-                fseek(source, 0, SEEK_SET);
-                while(! feof(source) )
-                {
-			tmp = getc(source);
-			
-
-			
-                    if( tmp == 124 )
-                    {
-                        ++occurence;
-                        if( occurence == *memptr )
-                            break;
-                    }
-                }
-
-                if( occurence == 0 || occurence != *memptr )
-                    fseek(source, old, SEEK_SET);
-
-                break;
-            }
-
-            case 94: // ^ seek
-                fseek(source, *memptr, SEEK_CUR);
-                break;
-
-            case 38: // & if zero, skip next instruction
-                if(!*memptr)
-                    fseek(source, 1, SEEK_CUR);
-                break;
-
-            case 96: // ` if zero, exit
-                if(!*memptr)
-                {
-                    cleanup();
-                    return 0;
-                }
-                break;
-
-            case 42: // * rand() < current value
-                *memptr = get_rand(*memptr);
-                break;
+			case 42: // * rand() < current value
+				*memptr = get_rand(*memptr);
+				break;
 
 			#ifdef DEBUG
-            case 84: // T stack trace
-                debug_print_trace();
-                break;
+			case 84: // T stack trace
+				debug_print_trace();
+				break;
 
 			case 77: // M memory map
 				debug_memorymap(memory);
@@ -200,59 +198,59 @@ int main( int argc, char* argv[] )
 			#endif
 		}
 
-        #ifdef DEBUG
-        struct Trace trace = { (int)ftell(source), buff, memptr, *memptr };
-        trace_push(trace);
-        #endif
+		#ifdef DEBUG
+		struct Trace trace = { (int)ftell(source), buff, memptr, *memptr };
+		trace_push(trace);
+		#endif
 
 		#ifdef SAFETY
-        /* Check position in memory */
+		/* Check position in memory */
 		if( memptr == &memory[ sizeof(memory) / sizeof(memory[0]) ] )
 		{
 			printf("\nFATAL: tried to access out-of-bounds memory!\n");
 
-            #ifdef DEBUG
-            debug_print_trace();
-            #endif
+			#ifdef DEBUG
+			debug_print_trace();
+			#endif
 
 			cleanup();
 			return 2;
 		}
 
-        /* Check for infinite loops (simple) */
-        if( buff == last && buff == 64 && lptr == memptr )
-        {
-            printf("\nFATAL: infinite loop!\n");
+		/* Check for infinite loops (simple) */
+		if( buff == last && buff == 64 && lptr == memptr )
+		{
+			printf("\nFATAL: infinite loop!\n");
 
-            #ifdef DEBUG
-            debug_print_trace();
-            #endif
+			#ifdef DEBUG
+			debug_print_trace();
+			#endif
 
-            cleanup();
-            return 3;
-        }
+			cleanup();
+			return 3;
+		}
 
-        /* Check for infinite loops (stack trace) */
-        if( buff == 64 )
-        {
-            for( int i = 0; i < 10; i += 2 )
-            {
-                if( stack_trace[i].pos == ftell(source) )
-                {
-                    printf("\nFATAL: infinite loop!\n");
+		/* Check for infinite loops (stack trace) */
+		if( buff == 64 )
+		{
+			for( int i = 0; i < 10; i += 2 )
+			{
+				if( stack_trace[i].pos == ftell(source) )
+				{
+					printf("\nFATAL: infinite loop!\n");
 
-                    #ifdef DEBUG
-                    debug_print_trace();
-                    #endif
+					#ifdef DEBUG
+					debug_print_trace();
+					#endif
 
-                    cleanup();
-                    return 3;
-                }
-            }
-        }
+					cleanup();
+					return 3;
+				}
+			}
+		}
 
-        last = buff;
-        lptr = memptr;
+		last = buff;
+		lptr = memptr;
 		#endif
 	}
 
--- trace.c
+++ trace.c
@@ -8,21 +8,21 @@
 #define __TRACE__
 
 struct Trace {
-    int       pos;
-    char      ins;
-    uint32_t* memptr;
-    int       memval;
+	int       pos;
+	char      ins;
+	uint32_t* memptr;
+	int       memval;
 };
 
 struct Trace stack_trace[5] = {};
 
 void trace_push(struct Trace trace)
 {
-    for( int i = 4; i > 0; --i )
-    {
-        stack_trace[i] = stack_trace[i-1];
-    }
-    stack_trace[0] = trace;
+	for( int i = 4; i > 0; --i )
+	{
+			stack_trace[i] = stack_trace[i-1];
+	}
+	stack_trace[0] = trace;
 }
 
 #endif
